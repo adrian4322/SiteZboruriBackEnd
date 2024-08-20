b47f9c49050350549a04c0df9b42b16aa00b4944
adrianB4322 <adrian.burlacu04@e-uvt.ro>
2024-08-20 19:33:02 +0300

    Modificare ResponseEntity in format json. A fost nevoie de aceasta modificare pentru ca Angular voia sa primeasca raspuns de tip json.

--- src/main/java/com/example/Site/models/AuthController.java
+++ src/main/java/com/example/Site/models/AuthController.java
@@ -3,9 +3,13 @@ package com.example.Site.models;
 import com.example.Site.repositories.UtilizatorRepository;
 
 import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -28,16 +32,23 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> logareUtilizator(@RequestBody LogareRequest logareRequest, HttpSession session) {
+    public ResponseEntity<Map<String, String>> logareUtilizator(@RequestBody LogareRequest logareRequest, HttpSession session) {
         Utilizator utilizator = utilizatorRepository.findByUsername(logareRequest.getUsername());
+        Map<String, String> raspuns = new HashMap<>();
+
+        System.out.println(logareRequest.getUsername() + " / " +  logareRequest.getPassword());
 
         if (utilizator == null || !passwordEncoder.matches(logareRequest.getPassword(), utilizator.getParola())) {
-            return ResponseEntity.status(401).body("eroare: Nume de utilizator sau parola gresite!");
+            raspuns.put("eroare", "Nume de utilizator sau parola gresite!");
+            return ResponseEntity.status(401).body(raspuns);
         }
 
         session.setAttribute("utilizator", utilizator);
 
-        return ResponseEntity.ok("Logare reusita!");
+        raspuns.put("Succes", " Logarea a fost cu succes!");
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .body(raspuns);
     }
 
 
@@ -49,25 +60,38 @@ public class AuthController {
     }
 
     @PostMapping("/creareCont")
-    public ResponseEntity<String> inregistrareUtilizator(@RequestBody CreareContRequest creareContRequest) {
-        if(utilizatorRepository.existsByUsername(creareContRequest.getUsername()))
+    public ResponseEntity<Map<String, String>> inregistrareUtilizator(@RequestBody CreareContRequest creareContRequest) {
+        Map<String, String> response = new HashMap<>();
+
+        if (utilizatorRepository.existsByUsername(creareContRequest.getUsername())) {
+            response.put("eroare:", " Numele de utilizator este luat!");
             return ResponseEntity
                     .badRequest()
-                    .body("eroare: Numele de utilizator este luat!");
+                    .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                    .body(response);
+        }
 
-        if(utilizatorRepository.existsByEmail(creareContRequest.getEmail()))
+        if (utilizatorRepository.existsByEmail(creareContRequest.getEmail())) {
+            response.put("eroare", " Email-ul este luat!");
             return ResponseEntity
                     .badRequest()
-                    .body("eroare: Email-ul este luat!");
+                    .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                    .body(response);
+        }
 
-        Utilizator utilizator = new  Utilizator(creareContRequest.getUsername(),
+        Utilizator utilizator = new Utilizator(
+                creareContRequest.getUsername(),
                 creareContRequest.getEmail(),
-                creareContRequest.getPassword());
+                creareContRequest.getPassword()
+        );
 
         utilizatorService.saveUtilizator(utilizator);
 
-        return ResponseEntity.ok("Utilizator creat!");
+        response.put("Succes", "Utilizator creat!");
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .body(response);
     }
-
-
 }
+
